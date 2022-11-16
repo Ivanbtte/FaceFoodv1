a79301d1d3fb0d50679cb45e49b0e5df8115e24e
Ivan <lupitobtte_17@hotmail.com>
2022-11-16 17:35:23 -0600

    Añadicion de clases prueba

---
+++ src/main/java/restaurante/model/Prueba.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package restaurante.model;
+
+import restaurante.entity.*;
+
+/**
+ *
+ * @author labso02
+ */
+public class Prueba {
+    
+}
